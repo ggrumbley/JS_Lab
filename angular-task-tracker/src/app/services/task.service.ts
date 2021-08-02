0d1f1d9603c8e9d47d4580f1c8ba0acf49a8b5f7
Gary Grumbley <ggrumbley@gmail.com>
2021-08-02 15:52:48 -0700

    feat(angular-task-tracker): add mock data and json server

---
+++ angular-task-tracker/src/app/services/task.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { Task } from '../types';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.apiUrl);
+  }
+
+  deleteTask(task: Task): Observable<Task> {
+    const taskPath = `${this.apiUrl}/${task.id}`;
+    return this.http.delete<Task>(taskPath);
+  }
+}
