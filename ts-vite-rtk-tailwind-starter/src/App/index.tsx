c2f127b8b933d5ba491bddf8ec3f9e8b7b17482f
Gary Grumbley <ggrumbley@gmail.com>
2021-08-27 13:40:51 -0700

    refactor(vite-react-template): fix breaking tests

--- ts-vite-rtk-tailwind-starter/src/App/index.tsx
+++ ts-vite-rtk-tailwind-starter/src/App/index.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Vite TypeScript React Redux Tailwind Boilerplate</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <Counter />
         <Pokemon />
