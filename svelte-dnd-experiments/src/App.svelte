edaab56ff6bb4c66b7c443efccf84069644c7dea
Gary Grumbley <ggrumbley@gmail.com>
2021-12-09 13:40:46 -0800

    chore(svelte-dnd-experiments): scaffold app

---
+++ svelte-dnd-experiments/src/App.svelte
@@ -0,0 +1,47 @@
+<script>
+</script>
+
+<main>
+  <div class="app-container">
+    <div class="game-screen">Game Screen</div>
+    <div class="player-actions">Actions</div>
+    <div>Inventory</div>
+  </div>
+</main>
+
+<style>
+  :root {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
+      'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  .app-container {
+    display: grid;
+    grid-template-areas:
+      'GameScreen GameScreen'
+      'PlayerActions Inventory';
+    grid-template-columns: 600px 600px;
+    grid-template-rows: 400px 205px;
+    gap: 3vh 1vw;
+    justify-content: center;
+    background-color: #fff;
+    padding-top: 3vh;
+  }
+
+  .game-screen {
+    grid-area: GameScreen;
+    background-color: aqua;
+  }
+
+  .player-actions {
+    grid-area: PlayerActions;
+    background-color: lightsalmon;
+  }
+</style>
