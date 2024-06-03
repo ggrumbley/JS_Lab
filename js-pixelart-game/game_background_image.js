d0bd88e641f7d0535490cec2d1a4b9383e8a3cad
Gary Grumbley <ggrumbley@classy.org>
2024-06-03 14:50:06 -0700

    feat(js-pixelart-game): add layered tile maps

--- js-pixelart-game/game_background_image.js
+++ js-pixelart-game/game_background_image.js
@@ -1,12 +1,7 @@
 import { Map } from './map.js';
 import { Camera } from './camera.js';
-// * Camera view width & height
 const GAME_WIDTH = 512;
 const GAME_HEIGHT = 512;
-
-// * Game view width & height
-// const GAME_WIDTH = 768;
-// const GAME_HEIGHT = 768;
 
 class Game {
   constructor() {
@@ -40,45 +35,29 @@ class Game {
     this.camera.move(deltaTime, speedX, speedY);
   }
 
-  drawLayer(layer, ctx) {
-    const startCol = Math.floor(this.camera.x / this.map.tileSize);
-    const endCol = startCol + this.camera.width / this.map.tileSize;
-    const startRow = Math.floor(this.camera.y / this.map.tileSize);
-    const endRow = startRow + this.camera.height / this.map.tileSize;
-    const offsetX = -this.camera.x + startCol * this.map.tileSize;
-    const offsetY = -this.camera.y + startRow * this.map.tileSize;
+  render(ctx) {
+    ctx.drawImage(
+      this.map.image,
+      this.camera.x,
+      this.camera.y,
+      GAME_WIDTH,
+      GAME_HEIGHT,
+      0,
+      0,
+      GAME_WIDTH,
+      GAME_HEIGHT,
+    );
 
-    for (let row = startRow; row <= endRow; row++) {
-      for (let col = startCol; col <= endCol; col++) {
-        const tile = this.map.getTile(layer, col, row);
-        const x = Math.round((col - startCol) * this.map.tileSize + offsetX);
-        const y = Math.round((row - startRow) * this.map.tileSize + offsetY);
-
-        ctx.drawImage(
-          this.map.image,
-          ((tile - 1) * this.map.image_tile) % this.map.image.width,
-          Math.floor((tile - 1) / this.map.image_cols) * this.map.image_tile,
-          this.map.image_tile,
-          this.map.image_tile,
-          x,
-          y,
+    for (let row = 0; row < this.map.rows; row++) {
+      for (let col = 0; col < this.map.rows; col++) {
+        ctx.strokeRect(
+          col * this.map.tileSize,
+          row * this.map.tileSize,
           this.map.tileSize,
           this.map.tileSize,
         );
-
-        //  * Game grid for debugging
-        // ctx.strokeRect(
-        //   col * this.map.tileSize,
-        //   row * this.map.tileSize,
-        //   this.map.tileSize,
-        //   this.map.tileSize,
-        // );
       }
     }
-  }
-  render(ctx) {
-    this.drawLayer(0, ctx);
-    this.drawLayer(1, ctx);
   }
 }
 
@@ -87,9 +66,9 @@ window.addEventListener('load', () => {
   const ctx = canvas.getContext('2d');
   canvas.width = GAME_WIDTH;
   canvas.height = GAME_HEIGHT;
-  ctx.imageSmoothingEnabled = false; // ! Keep off, blurs pixel art
 
   const game = new Game();
+  game.render(ctx);
 
   // * Game Loop
   let lastTime = 0;
